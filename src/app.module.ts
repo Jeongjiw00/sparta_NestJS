4d74fd57a203d391e568e2e3a38e02b9815be460
Jeongjiw00 <wldn0229@naver.com>
2023-02-22 16:00:51 +0900

    NestJS 심화 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { TypeOrmConfigService } from './config/typeorm.config.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // 일단 이것은 무조건 가장 위에서!
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     BoardModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
